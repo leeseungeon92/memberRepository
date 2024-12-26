f53578f3a4fad1327ac7fa9da9ea22a6910ca87e
selee <oksktmddjs@gmail.com>
2024-12-26 09:05:43 +0900

    로그인 기능

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.codingrecipe.member.controller;
 
 import com.codingrecipe.member.dto.MemberDTO;
 import com.codingrecipe.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +23,30 @@ public class MemberController {
     }
 
     @PostMapping("/member/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
-        return null;
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm(){
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+
+        System.out.println("memberDTO = " + memberDTO);
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            //login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            //login 실패
+            return "login";
+        }
     }
 }
